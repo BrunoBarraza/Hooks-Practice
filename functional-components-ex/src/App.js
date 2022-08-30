a536ee3b54c0e956733a9c4f470f466df2c4c676
brunobarraza <bruno.barraza@atos.net>
2022-08-30 16:26:00 -0500

    Class based component implementation

--- functional-components-ex/src/App.js
+++ functional-components-ex/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Component, useState } from "react";
 
 // Hooks rules:
 // Hooks cannot be called whitin loops. (if, while, etc.)
@@ -7,14 +7,31 @@ import { useState } from "react";
 // Hooks can only be called in Custom Hooks.
 // TIP: When creating a custom Hook, the naming should start with 'use'.
 
-const App = () => {
-  const [contador, setContador] = useState(0);
-  return (
-    <div>
-      Contador: {contador}
-      <button onClick={() => setContador(contador + 1)}>Incrementar</button>
-    </div>
-  );
-};
+// Class based component
+class App extends Component {
+  state = { contador: 0 };
+  incrementar = () => {
+    this.setState({ contador: this.state.contador + 1 });
+  };
+  render() {
+    return (
+      <div>
+        contador: {this.state.contador}
+        <button onClick={this.incrementar}>Incrementar</button>
+      </div>
+    );
+  }
+}
+
+// Functional Component
+// const App = () => {
+//   const [contador, setContador] = useState(0);
+//   return (
+//     <div>
+//       Contador: {contador}
+//       <button onClick={() => setContador(contador + 1)}>Incrementar</button>
+//     </div>
+//   );
+// };
 
 export default App;
