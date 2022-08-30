b9a1569d61dda0ed8ad91965325196533891ffae
brunobarraza <bruno.barraza@atos.net>
2022-08-30 16:04:50 -0500

    Impure and Pure function introduction

--- functional-components-ex/src/App.js
+++ functional-components-ex/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// Impure function: Constant Change - API, Database.
+const impure = () => new Date().toLocaleString();
+console.log(impure());
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Pure function: Never changes the output value - fixed value.
+const MyComponent = ({ myProp }) => {
+  return <div>Nombre: {myProp}</div>;
+};
+
+const App = () => {
+  return <MyComponent myProp={"chanchito feliz"} />;
+};
 
 export default App;
