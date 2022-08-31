08c1c288ecba2b77737c3c960bcb0e02233800e0
brunobarraza <bruno.barraza@atos.net>
2022-08-31 11:03:34 -0500

    Refactoring fields and adding new Field components with styling

--- calculator-project/src/App.js
+++ calculator-project/src/App.js
@@ -1,24 +1,32 @@
-import styled from "styled-components";
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  height: 100%;
-  align-items: center;
-`;
-
-const Section = styled.section`
-  background-color: #eee;
-  border-top: solid 2px palevioletred;
-  padding: 20px 25px;
-  width: 500px;
-  box-shadow: 0px 2px 3px rgb(0, 0, 0, 0.3);
-`;
+import { Formik, Form } from "formik";
+import Input from "./components/Input.js";
+import Button from "./components/Button.js";
+import Container from "./components/Container.js";
+import Section from "./components/Section.js";
 
 function App() {
+  const handleSubmit = () => {};
   return (
     <Container>
-      <Section>lala</Section>
+      <Section>
+        <Formik
+          initialValues={{
+            deposit: "",
+            contribution: "",
+            years: "",
+            rate: "",
+          }}
+          onSubmit={handleSubmit}
+        >
+          <Form>
+            <Input name="deposit" label="Depósito Inicial" />
+            <Input name="contribution" label="Contibución Anual" />
+            <Input name="years" label="Años" />
+            <Input name="rate" label="Interés Estimado" />
+            <Button>Calcular</Button>
+          </Form>
+        </Formik>
+      </Section>
     </Container>
   );
 }
