670d59a0f7aea48c5c549f5f148b80f50cb64b36
brunobarraza <bruno.barraza@atos.net>
2022-08-30 22:30:16 -0500

    refactoring the components within Formik form

--- formik-ex/src/App.js
+++ formik-ex/src/App.js
@@ -1,4 +1,4 @@
-import { Formik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 const validate = (values) => {
   const errors = {};
@@ -24,29 +24,25 @@ function App() {
       validate={validate}
       onSubmit={(values) => console.log(values)}
     >
-      {(formik) => (
-        <form onSubmit={formik.handleSubmit}>
-          <label>Nombre</label>
-          <input type="text" {...formik.getFieldProps("name")} />
-          {formik.touched.name && formik.errors.name ? (
-            <div>{formik.errors.name}</div>
-          ) : null}
-          <br />
-          <label>Apellido</label>
-          <input type="text" {...formik.getFieldProps("lastname")} />
-          {formik.touched.lastname && formik.errors.lastname ? (
-            <div>{formik.errors.lastname}</div>
-          ) : null}
-          <br />
-          <label>Email</label>
-          <input type="email" {...formik.getFieldProps("email")} />
-          {formik.touched.email && formik.errors.email ? (
-            <div>{formik.errors.email}</div>
-          ) : null}
-          <br />
-          <button type="submit">Enviar</button>
-        </form>
-      )}
+      <Form>
+        <label>Nombre</label>
+        <Field name="name" type="text" className="input" />
+        <Field name="select" as="select" className="input">
+          <option value="chanchitofeliz">Chanchito Feliz</option>
+          <option value="chanchitotriste">Chanchito Triste</option>
+        </Field>
+        <ErrorMessage name="name" />
+        <br />
+        <label>Apellido</label>
+        <Field name="lastname" type="text" />
+        <ErrorMessage name="lastname" />
+        <br />
+        <label>Email</label>
+        <Field name="email" type="email" />
+        <ErrorMessage name="email" />
+        <br />
+        <button type="submit">Enviar</button>
+      </Form>
     </Formik>
   );
 }
