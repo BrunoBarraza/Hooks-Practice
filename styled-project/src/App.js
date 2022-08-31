671c8b0f6cd039c6f44213b11afcd485f10370c2
brunobarraza <bruno.barraza@atos.net>
2022-08-31 09:14:37 -0500

    examples of component based styling

--- styled-project/src/App.js
+++ styled-project/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const P = styled.p`
+  font-size: 24px;
+  color: red;
+`;
+
+const Content = styled.section`
+  padding: 20px 25px;
+`;
+
+const Button = styled.button`
+  background-color: ${(props) => (props.primary ? "red" : "white")};
+  color: ${(props) => (props.primary ? "white" : "red")};
+  padding: 10px 15px;
+  border: solid 2px red;
+  border-radius: 4px;
+`;
+
+const BlockButton = styled(Button)`
+  width: 100%;
+  font-size: 24px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Content>
+      <P>Hola soy un parrafo</P>
+      <Button>Enviar</Button>
+      <Button primary>Enviar</Button>
+      <BlockButton primary as="a" href="#">
+        Enviar
+      </BlockButton>
+    </Content>
   );
 }
 
