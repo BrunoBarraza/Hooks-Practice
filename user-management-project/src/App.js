ba86d88fc6fb51d05dbb657620672b8cc0722dc2
brunobarraza <bruno.barraza@atos.net>
2022-08-30 21:02:11 -0500

    button component

--- user-management-project/src/App.js
+++ user-management-project/src/App.js
@@ -1,18 +1,53 @@
+import { useState } from "react";
 import userFormulario from "./hooks/useFormulario.js";
+import Input from "./components/Input.js";
+import Card from "./components/Card.js";
+import Container from "./components/Container.js";
+import Button from "./components/Button.js";
 
 function App() {
-  const [formulario, handleChange] = userFormulario({ name: "" });
+  const [usuarios, setUsuarios] = useState([]);
+  const [formulario, handleChange, reset] = userFormulario({
+    name: "",
+    lastname: "",
+    email: "",
+  });
 
-  console.log(formulario);
+  const submit = (e) => {
+    e.preventDefault();
+    setUsuarios([...usuarios, formulario]);
+    reset()
+  };
+
+  console.log(formulario, usuarios);
   return (
-    <form>
-      <input
-        name="name"
-        placeholder="Nombre"
-        value={formulario.name}
-        onChange={handleChange}
-      />
-    </form>
+    <Container>
+      <Card>
+        <div style={{ padding: 20 }}>
+          <form onSubmit={submit}>
+            <Input
+              label="Nombre"
+              name="name"
+              value={formulario.name}
+              onChange={handleChange}
+            />
+            <Input
+              label="Apellido"
+              name="lastname"
+              value={formulario.lastname}
+              onChange={handleChange}
+            />
+            <Input
+              label="Correo"
+              name="email"
+              value={formulario.email}
+              onChange={handleChange}
+            />
+            <Button>Enviar</Button>
+          </form>
+        </div>
+      </Card>
+    </Container>
   );
 }
 
