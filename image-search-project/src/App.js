7056bcf206bfe58028d9b479566bb4808471883f
brunobarraza <bruno.barraza@atos.net>
2022-08-31 08:30:05 -0500

    iterating the images

--- image-search-project/src/App.js
+++ image-search-project/src/App.js
@@ -4,6 +4,7 @@ import "./header.css";
 
 const App = () => {
   const [photos, setPhotos] = useState([]);
+  const open = (url) => window.open(url);
   console.log(photos);
   return (
     <section>
@@ -30,6 +31,16 @@ const App = () => {
           </Form>
         </Formik>
       </header>
+      <section className="container">
+        <div className="center">
+          {photos.map((photo) => (
+            <article key={photo.id} onClick={() => open(photo.links.html)}>
+              <img src={photo.urls.regular} alt="img" />
+              <p>{[photo.description, photo.alt_description].join(" - ")}</p>
+            </article>
+          ))}
+        </div>
+      </section>
     </section>
   );
 };
