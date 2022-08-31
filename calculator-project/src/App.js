1c27d69f181d9dd1612643985d044bb3526747a6
brunobarraza <bruno.barraza@atos.net>
2022-08-31 11:22:03 -0500

    showing the Balance, styling it and formating it

--- calculator-project/src/App.js
+++ calculator-project/src/App.js
@@ -1,11 +1,38 @@
+import { useState } from "react";
 import { Formik, Form } from "formik";
 import Input from "./components/Input.js";
 import Button from "./components/Button.js";
 import Container from "./components/Container.js";
 import Section from "./components/Section.js";
+import Balance from "./components/Balance.js";
+
+const compoundInterest = (deposit, contribution, years, rate) => {
+  let total = deposit;
+  for (let i = 0; i < years; i++) {
+    total = (total + contribution) * (rate + 1);
+  }
+
+  return Math.round(total);
+};
+
+const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
 
 function App() {
-  const handleSubmit = () => {};
+  const [balance, setBalance] = useState("");
+  const handleSubmit = ({ deposit, contribution, years, rate }) => {
+    const val = compoundInterest(
+      Number(deposit),
+      Number(contribution),
+      Number(years),
+      Number(rate)
+    );
+    setBalance(formatter.format(val));
+  };
   return (
     <Container>
       <Section>
@@ -23,9 +50,10 @@ function App() {
             <Input name="contribution" label="Contibución Anual" />
             <Input name="years" label="Años" />
             <Input name="rate" label="Interés Estimado" />
-            <Button>Calcular</Button>
+            <Button type="submit">Calcular</Button>
           </Form>
         </Formik>
+        {balance !== "" ? <Balance>Balance Final: {balance}</Balance> : null}
       </Section>
     </Container>
   );
