ac72c4d1bd521b9b122109a4c94dc7e96d463056
brunobarraza <bruno.barraza@atos.net>
2022-09-01 13:19:16 -0500

    connecting redux to react

--- redux-project/src/index.js
+++ redux-project/src/index.js
@@ -1,45 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createStore } from "redux";
-import App from "./App";
+import { Provider } from "react-redux";
+import App, { reducer } from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-// es un reducer!
-const store = createStore((state = 0, action) => {
-  // action = {type: 'tipo de accion', payload: any}
-  switch (action.type) {
-    case "incrementar": {
-      return state + 1;
-    }
-    case "decrementar": {
-      return state - 1;
-    }
-    case "set": {
-      return action.payload;
-    }
-    default:
-      return state;
-  }
-});
-
-console.log(store.getState());
-store.dispatch({ type: "lala" });
-
-console.log(store.getState());
-store.dispatch({ type: "incrementar" });
-
-console.log(store.getState());
-store.dispatch({ type: "decrementar" });
-
-console.log(store.getState());
-store.dispatch({ type: "set", payload: 15 });
-
-console.log(store.getState());
+const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
